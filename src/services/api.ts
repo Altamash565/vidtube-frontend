d6cc2c564a56a2ed28fb6de999ec647c70545341
Altamash Khan <altamashidrisi9090@gmail.com>
2026-06-25 20:56:10 +0530

    change the backend prod url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig, type AxiosError } from 'axios';
 
 const api = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL as string) || 'http://localhost:8000/api/v1',
+  baseURL: (import.meta.env.VITE_BACKEND_URL as string) || 'http://localhost:8000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
